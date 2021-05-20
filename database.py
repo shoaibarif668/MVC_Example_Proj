1dabc56f334f40ffe08d4f5924cda82a9c4d8cb2
Shoaib Arif <75606050+shoaibarif668@users.noreply.github.com>
2021-05-20 14:48:33 +0500

    Initial MVC Commit

--- database.py
+++ database.py
@@ -1,31 +1,24 @@
 employees = [
 
-{'EmployeelD' : 12345, 
+{'EmployeelD' : 1, 
 'FirstName' : "Mujtaba",
 'LastName' : 'Jafri',
 'Gender' : "Male",
-'HiredDate' : "12/2/2020",
+'HiredDate' : "2o/2/2020",
 'Salary' : 25000} ,
 
-{'EmployeelD' : 12346, 
+{'EmployeelD' : 2, 
 'FirstName' : "Dayal",
 'LastName' : 'Nasir',
 'Gender' : "Male",
-'HiredDate' : "12/2/2020",
+'HiredDate' : "2o/2/2020",
 'Salary' : 75000} ,
 
-{'EmployeelD' : 12347, 
+{'EmployeelD' : 3, 
 'FirstName' : "Shoaib",
 'LastName' : 'Arif',
-'Gender' : "Female",
-'HiredDate' : "12/2/2020",
-'Salary' : 30000} ,
-
-{'EmployeelD' : 12348, 
-'FirstName' : "Abdul",
-'LastName' : 'Haseeb',
 'Gender' : "Male",
-'HiredDate' : "12/2/2020",
-'Salary' : 50000} 
+'HiredDate' : "2o/2/2020",
+'Salary' : 30000} 
 
 ]
